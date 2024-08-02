aadfb5390ff3a8d296364dda195f13d54d92e77c
Michael McClimon <michael@mcclimon.org>
2024-08-01 21:07:55 -0400

    projects: add a --remote flag to fetch from toggl
    Sometimes I don't feel like going to track down the ID in the UI, so
    let's just make a command to get them.

--- cmd/projects.go
+++ cmd/projects.go
@@ -3,22 +3,32 @@ package cmd
 import (
 	"fmt"
 	"sort"
+	"strings"
 
 	"github.com/mmmcclimon/toggl-go/internal/toggl"
 	"github.com/spf13/cobra"
 	"golang.org/x/exp/maps"
 )
 
-type ProjectsCommand struct{}
+type ProjectsCommand struct {
+	remote bool
+}
 
 func (cmd *ProjectsCommand) Cobra() *cobra.Command {
-	return &cobra.Command{
+	cc := &cobra.Command{
 		Use:   "projects",
 		Short: "list the buckets things can go in",
 	}
+
+	cc.Flags().BoolVarP(&cmd.remote, "remote", "r", false, "fetch remote projects")
+	return cc
 }
 
 func (cmd *ProjectsCommand) Run(tc *toggl.Client, args []string) error {
+	if cmd.remote {
+		return cmd.listRemoteProjects(tc)
+	}
+
 	projects := tc.Config.ProjectShortcuts
 
 	shortcuts := maps.Keys(projects)
@@ -26,6 +36,27 @@ func (cmd *ProjectsCommand) Run(tc *toggl.Client, args []string) error {
 
 	for _, sc := range shortcuts {
 		fmt.Printf("- %s (%d)\n", sc, projects[sc])
+	}
+
+	return nil
+}
+
+func (cmd *ProjectsCommand) listRemoteProjects(tc *toggl.Client) error {
+	projects, err := tc.Projects()
+	if err != nil {
+		return err
+	}
+
+	wrap := func(s string) string {
+		if strings.Contains(s, " ") {
+			return fmt.Sprintf(`"%s"`, s)
+		}
+
+		return s
+	}
+
+	for _, p := range projects {
+		fmt.Printf("%s = %d\n", wrap(p.Name), p.Id)
 	}
 
 	return nil
